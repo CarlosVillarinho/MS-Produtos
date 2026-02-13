a256e0f0ce7fb2ab1f4581863c5c2a18dfdbaed5
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-13 08:23:52 -0300

    fist commit

---
+++ src/main/java/com/github/carlosvillarinho/ms/produto/MsProdutoApplication.java
@@ -0,0 +1,14 @@
+package com.github.carlosvillarinho.ms.produto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProdutoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MsProdutoApplication.class, args);
+	}
+
+}
